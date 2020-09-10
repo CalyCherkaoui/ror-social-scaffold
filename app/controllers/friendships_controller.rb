71ee55e1bf7a83a306c7e63530d4af30b6d55d64
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-10 23:47:18 +0300

    Add actions for index, update and destroy

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,10 @@
 class FriendshipsController < ApplicationController
 
+  def index
+    @pending_friends = current_user.pending_friends
+    @friend_requests = current_user.friend_requests
+  end
+
   def create
     user = User.find(params[:id])
     if user
@@ -10,14 +15,18 @@ class FriendshipsController < ApplicationController
   end
 
   def update
-
+    @friendship = Friendship.find(params[:id])
+    @friendship.update(confirmed: true)
+    redirect_to friendships_path
   end
 
   def show
 
   end
 
-  def delete
-
+  def destroy
+    @friendship = Friendship.find(params[:id])
+    @friendship.destroy
+    redirect_to friendships_path
   end
 end
