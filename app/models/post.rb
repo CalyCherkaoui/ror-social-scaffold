1fd18775ca5a6a90597197a7e1ac11845771af87
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-11 20:00:44 +0300

    Add scope to return only current user and friends posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  scope :desc_current_user_and_friends, ->(current_user) { where(user: current_user.friends << current_user).order(created_at: :desc) }
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
