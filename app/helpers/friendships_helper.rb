b7c6113df357e9fcca06d3e338419565674e2f89
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-11 20:11:34 +0300

    Fix linter errors

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -8,13 +8,15 @@ module FriendshipsHelper
 
       if show_add_friend(user)
         show_friend += link_to('Invite to friendship',
-                               { controller: 'friendships', action: 'create', id: user.id }, method: :post, class: 'profile-link')
+                               { controller: 'friendships', action: 'create', id: user.id },
+                               method: :post,
+                               class: 'profile-link')
       end
 
       show_already_friend = '<span> | you are friends</span>' if show_already_friend(user)
     end
 
-    ('<li>'.html_safe + namespan.html_safe + link_to('See profile', user_path(user), class: 'profile-link')
-     + show_friend.html_safe + show_already_friend.html_safe + '</span>'.html_safe)
+    final_string = ('<li>'.html_safe + namespan.html_safe + link_to('See profile', user_path(user), class: 'profile-link')
+                    + show_friend.html_safe + show_already_friend.html_safe + '</span>'.html_safe)
   end
 end
