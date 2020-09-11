a82b2e4c30ae446051fe20573d57a599ab3274bb
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-11 11:31:09 -0500

    Adding pending request helper

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,2 +1,24 @@
 module FriendshipsHelper
+
+  def pending_requests_helper (user, current_user)
+
+    namespan = "Name: #{user.name}" + "<span class=\"profile-link\">"
+    show_friend = ''
+    show_already_friend = ''
+
+    if current_user != User.find(user.id)
+
+      if show_add_friend(user)
+        show_friend = show_friend + link_to('Invite to friendship',
+         {:controller => 'friendships', :action => 'create', :id => user.id}, method: :post, class: 'profile-link')
+      end
+
+      if show_already_friend(user)
+        show_already_friend = '<span> | you are friends</span>'
+      end
+    end
+
+    ('<li>'.html_safe + namespan.html_safe + link_to('See profile',  user_path(user), class: 'profile-link')
+     + show_friend.html_safe + show_already_friend.html_safe + '</span>'.html_safe)
+  end
 end
