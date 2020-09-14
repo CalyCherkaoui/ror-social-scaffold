eb14aa428923ec12ffc6b01beeb5952a5c703ebf
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-14 23:24:15 +0300

    Fix error in test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   context 'creating valid posts' do
-    it 'A post with contentent and valid user should be valid' do
-      user = User.new(name: 'usertest2', email: 'usertest2@test.com', password: 'foobar', password_confirmation: 'foobar').save
-      post = Post.new(user_id: user, content: 'Foobar').save
-      # post = user.posts.Create!(content: 'Foobar')
-      expect(post).to eq(true)
+    it 'A post with content and valid user should be valid' do
+      user = User.new(name: 'usertest2', email: 'usertest2@test.com', password: 'foobar', password_confirmation: 'foobar')
+      user.save
+
+      post = Post.new(user: user, content: 'Foobar')
+
+      expect(post.save).to eq(true)
     end
   end
 end
