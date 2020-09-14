a90093f9f1e6e7a1975c99178c0ac199bd2a10c7
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-14 16:02:02 -0500

    Adding integration test for Sign in in User

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  describe "the sign in process" do
+    before :each do
+      User.new(name: 'usertest',
+            email: 'usertest@test.com',
+            password: 'foobar',
+            password_confirmation: 'foobar').save
+    end
+
+    it "signs me in should succeed" do
+      visit new_user_session_path
+      within("form") do
+        fill_in 'Email', with: 'usertest@test.com'
+        fill_in 'Password', with: 'foobar'
+      end
+      click_button 'Log in'
+      expect(page).to have_content 'Signed in successfully.'
+    end
+
+    it "signs me in should fail" do
+      visit new_user_session_path
+      within("form") do
+        fill_in 'Email', with: 'failed@test.com'
+        fill_in 'Password', with: 'foobar'
+      end
+      click_button 'Log in'
+      expect(page).to have_content 'Invalid Email or password'
+    end
+  end
+
+
+end
