6348c71d0b18e3d99c1fec8dd67333571cbe2d03
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-14 23:17:14 +0300

    Create unit test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'content should be present' do
+      user1 = User.new(name: 'usertest', email: 'usertest@test.com', password: 'foobar', password_confirmation: 'foobar')
+      user1.save
+
+      user2 = User.new(name: 'usertest2', email: 'usertest@test2.com', password: 'foobar2', password_confirmation: 'foobar2')
+      user2.save
+
+      post1 = Post.new(content: 'test post', user: user1)
+      post1.save
+
+      comment = Comment.new(content: 'test comment')
+      comment.post_id = post1.id
+      comment.user = user2
+
+      expect(comment.save).to eq(true)
+    end
+  end
+
+  context 'associations' do
+    it 'should belong to a user' do
+      comment = Comment.reflect_on_association(:user)
+      expect(comment.macro).to eq(:belongs_to)
+    end
+    it 'should belong to a post' do
+      comment = Comment.reflect_on_association(:post)
+      expect(comment.macro).to eq(:belongs_to)
+    end
+  end
 end
