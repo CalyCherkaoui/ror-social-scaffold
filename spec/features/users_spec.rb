913865aa816c5a32001c0ca37a5e20a32e5f13bc
Asiimwe Dismas <asiimwedismas@gmail.com>
2020-09-15 20:08:30 +0300

    Refactor

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -31,26 +31,24 @@ RSpec.feature 'Users', type: :feature do
   end
 
   scenario 'user seding a friendship request to a friend that will accept it' do
-
     User.new(name: 'usertest',
-               email: 'usertest@test.com',
-               password: 'foobar',
-               password_confirmation: 'foobar').save
+             email: 'usertest@test.com',
+             password: 'foobar',
+             password_confirmation: 'foobar').save
     User.new(name: 'friendtest',
-               email: 'friendtest@test.com',
-               password: 'foobar',
-               password_confirmation: 'foobar').save
+             email: 'friendtest@test.com',
+             password: 'foobar',
+             password_confirmation: 'foobar').save
 
-      visit new_user_session_path
-      within('form') do
-          fill_in 'Email', with: 'usertest@test.com'
-          fill_in 'Password', with: 'foobar'
-      end
-      click_button 'Log in'
-      visit users_path
-      click_link 'Invite to friendship'
-      visit friendships_path
-      expect(page).to have_content('friendtest')
-
+    visit new_user_session_path
+    within('form') do
+      fill_in 'Email', with: 'usertest@test.com'
+      fill_in 'Password', with: 'foobar'
+    end
+    click_button 'Log in'
+    visit users_path
+    click_link 'Invite to friendship'
+    visit friendships_path
+    expect(page).to have_content('friendtest')
   end
 end
