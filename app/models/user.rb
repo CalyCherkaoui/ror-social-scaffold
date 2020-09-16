b647e9dbc399ef6405ef5f92fa9736501d041004
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2020-09-16 14:56:46 -0500

    Updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   has_many :friendships, dependent: :destroy
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id', dependent: :destroy
 
+
   def friends
     friends_array = friendships.map { |f| f.friend if f.confirmed }
     friends_array.concat(inverse_friendships.map { |f| f.user if f.confirmed })
@@ -19,20 +20,20 @@ class User < ApplicationRecord
   end
 
   # Users who have yet to confirme friend requests
-  # def pending_friends
-  #   friendships.map { |f| f.friend unless f.confirmed }.compact
-  # end
+  def pending_friends
+    friendships.map { |f| f.friend unless f.confirmed }.compact
+  end
 
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
-  has_many :pending_friends, through: :pending_friendships, source: :friend
+  # has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  # has_many :pending_friends, through: :pending_friendships, source: :friend
 
   # Users who have requested to be friends
-  # def friend_requests
-  #   inverse_friendships.map { |f| f.user unless f.confirmed }.compact
-  # end
+  def friend_requests
+    inverse_friendships.map { |f| f.user unless f.confirmed }.compact
+  end
 
-  has_many :friendship_requests, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
-  has_many :friend_requests, through: :friendship_requests, source: :friend
+  # has_many :friendship_requests, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  # has_many :friend_requests, through: :friendship_requests, source: :friend
 
   def confirm_friend(user)
     friendship = inverse_friendships.find { |f| f.user == user }
